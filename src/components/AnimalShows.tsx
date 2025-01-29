8c29ecd4a644826afd58b789e9651f0179e37061
Tarachand <tarachand90@gmail.com>
2025-01-29 12:32:41 +0530

    Addig first eventlistner syntax

---
+++ src/components/AnimalShows.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+interface AnimalType {
+    type: string;
+}
+const AnimalShows = ({type}: AnimalType) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AnimalShows
