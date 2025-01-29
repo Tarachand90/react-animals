9b94fb433f91046798a3044a04fe6be1a668a698
Tarachand <tarachand90@gmail.com>
2025-01-29 16:21:01 +0530

    Moved the constans value to different module

--- src/components/AnimalShows.tsx
+++ src/components/AnimalShows.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import bird from '../images/bird.svg';
-import cat from '../images/cat.svg';
-import cow from '../images/cow.svg';
-import dog from '../images/dog.svg';
-import gator from '../images/gator.svg';
-import horse from '../images/horse.svg';
 
-type AnimalKey = 'bird' | 'cat' | 'cow' | 'dog' | 'gator' | 'horse';
+import { AnimalKey, animalsMap } from '../constants/animalsMap';
 
 interface AnimalType {
     type: AnimalKey;
 }
-
-const animalsMap: Record<AnimalKey, string> = {
-  bird,
-  cat,
-  cow,
-  dog,
-  gator,
-  horse
-};
 
 const AnimalShows = ({type}: AnimalType) => {
   return (
