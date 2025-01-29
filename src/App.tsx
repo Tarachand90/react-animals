976dc41e8f00e64509919d0184f61a798ea3d6ce
Tarachand <tarachand90@gmail.com>
2025-01-29 13:54:58 +0530

    Defined set of animals which will be picked randomly when user click the button and it will display in the screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const getRandomElement = ():string => {
+    const animals:string[] = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+  }
+  
+  const [animals, setAnimals] = useState<string[]>([]);
+
   const handleClick = (): void => {
-    console.log('Button was clicked!')
+    setAnimals([...animals, getRandomElement()]);
   }
 
   return (
     <>
       <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
     </>
   )
 }
