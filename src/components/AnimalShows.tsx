5f89826ac70e6a74ad3fa6c0970e8d76f1f03c8a
Tarachand <tarachand90@gmail.com>
2025-01-29 15:47:05 +0530

    When user clicked add animal button, it will display a random image

--- src/components/AnimalShows.tsx
+++ src/components/AnimalShows.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+import bird from '../images/bird.svg';
+import cat from '../images/cat.svg';
+import cow from '../images/cow.svg';
+import dog from '../images/dog.svg';
+import gator from '../images/gator.svg';
+import horse from '../images/horse.svg';
+
+type AnimalKey = 'bird' | 'cat' | 'cow' | 'dog' | 'gator' | 'horse';
 
 interface AnimalType {
-    type: string;
+    type: AnimalKey;
 }
+
+const animalsMap: Record<AnimalKey, string> = {
+  bird: bird,
+  cat: cat,
+  cow: cow,
+  dog: dog,
+  gator: gator,
+  horse: horse
+};
+
 const AnimalShows = ({type}: AnimalType) => {
   return (
     <div>
-      {type}
+      <img alt={type} src={animalsMap[type]} />
     </div>
   )
 }
